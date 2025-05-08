4fab6ab3173d1c619ec3f9ef29fdf20b1cf6d79c
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-05-08 18:00:41 -0400

    begin subsystem commands

---
+++ TeamCode/src/main/java/teamcode/robot/commands/subsystemcommands/PivotTo.java
@@ -0,0 +1,11 @@
+package teamcode.robot.commands.subsystemcommands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import teamcode.robot.Robot;
+
+public class PivotTo extends CommandBase {
+    public PivotTo(Robot robot){
+        robot.
+    }
+}
